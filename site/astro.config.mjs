23f969b091164ed7e27b5cf8e61b4e30b756e8be
Mike Kidushim <mike@zorakle.net>
2025-12-21 08:35:56 -0700

    first deploy

---
+++ site/astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+
+import tailwindcss from "@tailwindcss/vite";
+
+
+export default defineConfig({
+  site: "https://mkidushim.github.io",
+  base: "/joe_kidushim/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
