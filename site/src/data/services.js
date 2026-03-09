666f2efaa1b35025e4c58104a4192a4602c6f15e
Mike Kidushim <mike@zorakle.net>
2026-03-09 14:16:33 -0600

    cleanup almost finished

--- site/src/data/services.js
+++ site/src/data/services.js
@@ -1,9 +1,16 @@
+import family from '../assets/family-protection.webp';
+import wealth from '../assets/wealth.webp';
+import succession from '../assets/succession-planning.webp';
+import executive from '../assets/executive-benefits.webp';
+import longterm from '../assets/long-term-care.webp';
+import life from '../assets/life-insurance.webp';
+
 export const services = [
     {
         "link": "family-protection",
         "title": "Family Protection",
         'subtitle': 'Plan for the ones you love',
-        'image': '',
+        'image': family,
         'description': 'Provide for your family no matter what life brings your way with a solid foundation of coverage.',
         "full_description": "We all want to know that our families will be provided for and protected. Life and disability insurance ensures that your family’s financial needs and obligations will be met in the event of an emergency."
     },
@@ -11,7 +18,7 @@ export const services = [
         "link": "wealth-preservtion",
         "title": "Wealth preservation",
         'subtitle': 'Shelter your assets from the tax storm',
-        'image': '',
+        'image': wealth,
         'description': 'Give you and your loved ones the peace of mind that your assets are protected from exorbitant fees and taxes.',
         "full_description": "There’s nothing like the peace of mind of preserving your hard-earned wealth for the ones you love. We believe in thoughtfully considering all the factors at play to help your legal and accounting teams create the solutions that will meet your objectives."
     },
@@ -19,7 +26,7 @@ export const services = [
         "link": "succession-planning",
         "title": "Business Succession Planning",
         'subtitle': 'Take care of the future, today',
-        'image': '',
+        'image': succession,
         'description': 'Make complex transitions easy on colleagues and family with coverage that insulates everyone.',
         "full_description": "After building an empire why leave its success to chance? We work closely with businesses of all sizes and their executives to put plans in place for when there is an unexpected death. This gives you, your business partners, and your families the resources to cover cash flow, the ability to buy or sell the business, or hire the highly skilled employees needed to keep operations running smoothly."
     },
@@ -27,7 +34,7 @@ export const services = [
         "link": "executive-benefits",
         "title": "Executive & Retirement Benefits",
         'subtitle': 'Go above and beyond vested',
-        'image': '',
+        'image': executive,
         'description': 'Prepare now for the next chapter of your life and elevate your golden years.',
         "full_description": "There’s more to traditional executive benefits than meets the eye. We help our clients complement 401 (k) or defined benefit pension plans that benefit both the business and its executives. With an eye towards tax efficiency, we look for ways to make contributions tax-deductible while having these contributions grow tax-deferred."
     },
@@ -35,7 +42,7 @@ export const services = [
         "link": "long-term-care",
         "title": "Long Term Care",
         'subtitle': 'Maintain your dignity and your life savings',
-        'image': '',
+        'image': longterm,
         'description': 'Maintain your dignity and your life savings with coverage that provides you with quality care.',
         "full_description": "If you have ever watched a loved one struggle with prolonged illness, a life-changing illness, or cognitive decline, you already know the need for long-term care coverage. A critical estate planning tool, these policies can include the in-home care, skilled nursing, and daily assistance you may one day need. Premiums may be tax-deductible and benefits are always tax-free."
     },
@@ -43,7 +50,7 @@ export const services = [
         "link": "life-insurance",
         "title": "Life Insurance Settlements",
         'subtitle': 'Maximize the value of a policy you no longer need',
-        'image': '',
+        'image': life,
         'description': 'Be matched with the quality buyers that can purchase the policies you no longer need.',
         "full_description": "It’s a little-known fact that if you have a policy you no longer need or want, you can sell it. We can help match you with qualified buyers to secure significantly more cash than just canceling it."
     },
